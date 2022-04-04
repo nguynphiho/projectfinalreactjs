054a1b06092d9984055e336130b5dc8068b474ef
unknown <toan8thang@gmail.com>
2022-04-04 14:11:04 +0700

    comment feature

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -1,12 +1,17 @@
 import Footer from "pages/Footer/Footer";
 import Header from "pages/Header/Header";
 import { Outlet } from "react-router-dom";
+import Comments from "./Comment/Comments";
+import Slider from "./Slider";
+
 
 const Customer = () => {
   return (
     <div>
       <Header />
+      <Slider/>
       <Outlet />
+      <Comments currentUserId="1"/>
       <Footer />
     </div>
   );
